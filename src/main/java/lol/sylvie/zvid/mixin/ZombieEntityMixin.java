038c996ec2845f66a701fa661e61bbfe21ef617a
sylvie 3 <me@sylvie.lol>
2025-12-14 11:46:19 -0600

    mojmap, 1.21.11

--- src/main/java/lol/sylvie/zvid/mixin/ZombieEntityMixin.java
+++ src/main/java/lol/sylvie/zvid/mixin/ZombieEntityMixin.java
@@ -1,16 +1,16 @@
 package lol.sylvie.zvid.mixin;
 
-import net.minecraft.entity.mob.ZombieEntity;
-import net.minecraft.server.world.ServerWorld;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.Difficulty;
+import net.minecraft.world.entity.monster.zombie.Zombie;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(ZombieEntity.class)
+@Mixin(Zombie.class)
 public class ZombieEntityMixin {
-    @Redirect(method = "onKilledOther", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/world/ServerWorld;getDifficulty()Lnet/minecraft/world/Difficulty;"))
-    public Difficulty zvid$redirectDifficulty(ServerWorld instance) {
+    @Redirect(method = "killedEntity", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ServerLevel;getDifficulty()Lnet/minecraft/world/Difficulty;"))
+    public Difficulty zvid$redirectDifficulty(ServerLevel instance) {
         return Difficulty.HARD;
     }
 }
